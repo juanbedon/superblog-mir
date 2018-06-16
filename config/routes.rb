f79ed50b590adb218e5676761fef4280b8655575
Juan <juanbedon26@hotmail.com>
2018-06-16 15:39:32 -0500

    3. Authentication Pt.1, added edit and remove functionality and my_posts page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
 
-	get 'about' => 'pages#about', as: :about
-	get 'contact' => 'pages#contact', as: :contact
+	root to: 'blog/posts#index'
 
-	root to: 'posts#index'
-  resources :posts
+	namespace :user do
+  	resources :posts
+	end
+
+	scope module: 'blog' do
+
+		get 'about' => 'pages#about', as: :about
+		get 'contact' => 'pages#contact', as: :contact
+		get 'posts' => 'posts#index', as: :posts
+		get 'posts/:id' => 'posts#show', as: :post
+
+	end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
